ceaf35db6ffeb92bcdf9593012b496c417c6d2e6
bunny vishal <bunny vishal>
2016-07-30 20:48:29 +0530

    Improved UI

--- public/app.js
+++ public/app.js
@@ -6,17 +6,44 @@
         $interpolateProvider.endSymbol('}]}');
     });
 
-    app.controller("FormCtrl", function () {
+    var name, pictureUrl, email;
+
+
+
+
+    app.controller("newPollOptionsCtrl", function () {
         var num = 2;
-        this.options = ["option1", "option2"];
+        this.options = [];
+        for(var i = 1; i <= num; i++){
+            this.options.push({name: "option", num:i});
+        }
         this.addOption = function () {
-            if (num < 10) {
-                num += 1;
-                this.options.push("option" + num);
+            if (this.options.length < 10) {
+                var currNum = this.options.length + 1;
+                this.options.push({name: "option", num:currNum});
             } else {
                 alert("Oops you cannot have more than 10 options for a poll");
             }
         };
+        this.removeOption = function(option){
+            var position = this.options.indexOf(option)
+            if(this.options.length > 2){
+                num -= 1;
+                var removed = this.options.splice(position, 1);
+            } else {
+                alert("Your poll should have atleast two options");
+            }
+        }
+    });
+
+
+    app.directive("newPollOptions", function(){
+        return{
+            restrict: "E",
+            templateUrl: 'new-poll-options.html',
+            controller: 'newPollOptionsCtrl',
+            controllerAs: 'poll'
+        }
     });
 
 })();
